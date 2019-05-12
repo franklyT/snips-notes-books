041ddbb52d1ac5854dc985fb606118e0af7ed2e8
maldici <48483484+maldici@users.noreply.github.com>
2019-05-12 12:39:41 -0400

    Create code.js

---
+++ books/EloquentJavaScript-MarjiHaverbeke/CH1/code.js
@@ -0,0 +1,16 @@
+console.log(typeof 4.5)
+// number
+console.log(typeof "x")
+// string
+
+
+console.log("Aardvark" < "Zoroaster")
+// true
+
+console.log(NaN == NaN)
+// false
+
+console.log(true ? 1: 2);
+// 1
+console.log(false ? 1: 2);
+// 2
