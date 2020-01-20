e7fa07c1dc87eed6682563c233fc9334343c22b7
franklyT <48483484+franklyT@users.noreply.github.com>
2020-01-20 06:43:39 -0500

    Update exercises.py

--- books/AutomatetheBoringStuff/CH03/exercises.py
+++ books/AutomatetheBoringStuff/CH03/exercises.py
@@ -13,3 +13,21 @@ def collatz(number):
 
 numberInput = input("Enter number: ")
 collatz(numberInput)
+
+# less pure function
+
+def collatz(n):
+    number = int(n)
+    if number % 2 == 0:
+        val = int(number/2)
+        print(val)
+        return val
+    else:
+        val = int(3*number+1)
+        print(val)
+        return val
+
+numberInput = input("Enter number: ")
+
+while numberInput != 1:
+    numberInput = collatz(numberInput)
